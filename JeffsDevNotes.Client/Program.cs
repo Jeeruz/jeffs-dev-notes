feb41ea254cf5e30dee2ba26aa00ffa63addc50a
Jefferson Dela Cruz <jdelacruz.proacc@gmail.com>
2026-08-21 05:07:23 +0800

    feat: blazorstrap v5 installed

--- JeffsDevNotes.Client/Program.cs
+++ JeffsDevNotes.Client/Program.cs
@@ -1,5 +1,7 @@
+using BlazorStrap;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazorStrap();
 
 await builder.Build().RunAsync();
